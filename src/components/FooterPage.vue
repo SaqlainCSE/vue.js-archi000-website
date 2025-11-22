750f3acebce36ecd73b28f96b92c1e01dfb5c329
Workstation <saqlaincse@gmail.com>
2025-11-22 14:04:10 +0600

    footer updated

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -51,7 +51,7 @@
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Developed By <a href="https://saqlain.pythonanywhere.com/">Saqlain Mustak</a>
+                        Developed By <a href="https://saqlain.pythonanywhere.com/" target="_blank">Saqlain Mustak</a>
                         <br> Maintained By: <a class="border-bottom" href="https://saqlain.pythonanywhere.com/" target="_blank">TECH000</a>
                     </div>
                 </div>
